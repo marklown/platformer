eb353c7b1b2bb77cedc5792020f3bbe5f07dd0ea
Mark Lown <lown.mark@gmail.com>
2018-10-31 22:22:59 -0700

    - Read level from file

--- source/main.c
+++ source/main.c
@@ -3,22 +3,24 @@
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 
-#define PLAYER_X_VEL     200
-#define PLAYER_X_ACC     500
-#define PLAYER_X_DEC     1000
-#define PLAYER_JUMP_VEL  400
-#define GRAVITY          1000
-#define SIZE_X           20
-#define SIZE_Y           15
+static const float PLAYER_X_VEL = 200;
+static const float PLAYER_X_ACC = 500;
+static const float PLAYER_X_DEC = 600;
+static const float PLAYER_JUMP_VEL = 450;
+static const float GRAVITY = 1000;
 
-static const Uint8 BELOW_C = 0;
-static const Uint8 ABOVE_C = 1;
+static const Uint8 SIZE_X = 20;
+static const Uint8 SIZE_Y = 15;
+
+static const Uint8 DOWN = 0;
+static const Uint8 UP = 1;
 static const Uint8 LEFT = 2;
 static const Uint8 RIGHT = 3;
-static const Uint8 ABOVE_L = 4;
-static const Uint8 ABOVE_R = 5;
-static const Uint8 BELOW_L = 6;
-static const Uint8 BELOW_R = 7;
+static const Uint8 UP_LEFT = 4;
+static const Uint8 UP_RIGHT = 5;
+static const Uint8 DOWN_LEFT = 6;
+static const Uint8 DOWN_RIGHT = 7;
+static const Uint8 NONE = 255;
 
 typedef struct Player
 {
@@ -27,6 +29,8 @@ typedef struct Player
     float vy;
     float ax;
     bool isOnGround;
+    bool canJumpAgain;
+    Uint8 direction;
 } Player;
 
 typedef struct Tile
@@ -40,6 +44,9 @@ SDL_Renderer* renderer = NULL;
 
 Player player;
 Tile map[SIZE_X][SIZE_Y];
+
+static FILE* mapFile = NULL;
+static bool run = true;
 
 void DrawDebugRect(const SDL_Rect* rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 {
@@ -70,14 +77,16 @@ bool Collides(const SDL_Rect* a, const SDL_Rect* b)
 void InitPlayer()
 {
     memset(&player, 0, sizeof(Player));
-    player.rect.x = 10 * 32;
-    player.rect.y = SIZE_Y * 32 - 32 * 2 + 10;
-    player.rect.w = 32;
-    player.rect.h = 32;
+    player.rect.x = 0;
+    player.rect.y = 0;
+    player.rect.w = 28;
+    player.rect.h = 28;
     player.vx = 0;
     player.vy = 0;
     player.ax = 0;
     player.isOnGround = false;
+    player.canJumpAgain = true;
+    player.direction = DOWN;
 }
 
 void ResolveXCollisions()
@@ -85,14 +94,14 @@ void ResolveXCollisions()
     int mapX = (player.rect.x + 16) / 32;
     int mapY = (player.rect.y + 16) / 32;
     Tile* sorted[8];
-    sorted[BELOW_C] = &map[mapX][mapY+1];
-    sorted[ABOVE_C] = &map[mapX][mapY-1];
+    sorted[DOWN] = &map[mapX][mapY+1];
+    sorted[UP] = &map[mapX][mapY-1];
     sorted[LEFT] = &map[mapX-1][mapY];
     sorted[RIGHT] = &map[mapX+1][mapY];
-    sorted[ABOVE_L] = &map[mapX-1][mapY-1];
-    sorted[ABOVE_R] = &map[mapX+1][mapY-1];
-    sorted[BELOW_L] = &map[mapX-1][mapY+1];
-    sorted[BELOW_R] = &map[mapX+1][mapY+1];
+    sorted[UP_LEFT] = &map[mapX-1][mapY-1];
+    sorted[UP_RIGHT] = &map[mapX+1][mapY-1];
+    sorted[DOWN_LEFT] = &map[mapX-1][mapY+1];
+    sorted[DOWN_RIGHT] = &map[mapX+1][mapY+1];
     for (int i=0;i<8;i++) {
         if (sorted[i]->isSolid) {
             if (Collides(&player.rect, &sorted[i]->rect)) {
@@ -113,14 +122,14 @@ void ResolveYCollisions()
     int mapX = (player.rect.x + 16) / 32;
     int mapY = (player.rect.y + 16) / 32;
     Tile* sorted[8];
-    sorted[BELOW_C] = &map[mapX][mapY+1];
-    sorted[ABOVE_C] = &map[mapX][mapY-1];
+    sorted[DOWN] = &map[mapX][mapY+1];
+    sorted[UP] = &map[mapX][mapY-1];
     sorted[LEFT] = &map[mapX-1][mapY];
     sorted[RIGHT] = &map[mapX+1][mapY];
-    sorted[ABOVE_L] = &map[mapX-1][mapY-1];
-    sorted[ABOVE_R] = &map[mapX+1][mapY-1];
-    sorted[BELOW_L] = &map[mapX-1][mapY+1];
-    sorted[BELOW_R] = &map[mapX+1][mapY+1];
+    sorted[UP_LEFT] = &map[mapX-1][mapY-1];
+    sorted[UP_RIGHT] = &map[mapX+1][mapY-1];
+    sorted[DOWN_LEFT] = &map[mapX-1][mapY+1];
+    sorted[DOWN_RIGHT] = &map[mapX+1][mapY+1];
     for (int i=0;i<8;i++) {
         if (sorted[i]->isSolid) {
             if (Collides(&player.rect, &sorted[i]->rect)) {
@@ -141,14 +150,24 @@ void ResolveYCollisions()
 
 void UpdatePlayerPhysics(float dt)
 {
-    // Compute player physics
     player.vx += player.ax * dt;
+
     if (player.vx > PLAYER_X_VEL) {
         player.vx = PLAYER_X_VEL;
-    }
-    if (player.vx < -PLAYER_X_VEL) {
+    } else if (player.vx < -PLAYER_X_VEL) {
         player.vx = -PLAYER_X_VEL;
     }
+
+    if (player.direction == RIGHT && player.ax < 0 && player.vx <= 0) {
+        player.vx = 0;
+        player.ax = 0;
+        player.direction = NONE;
+    } else if (player.direction == LEFT && player.ax > 0 && player.vx >= 0) {
+        player.vx = 0;
+        player.ax = 0;
+        player.direction = NONE;
+    }
+
     player.rect.x += player.vx * dt;
 
     ResolveXCollisions();
@@ -158,6 +177,12 @@ void UpdatePlayerPhysics(float dt)
     player.rect.y += player.vy * dt;
 
     ResolveYCollisions();
+
+
+    if (player.rect.y >= SIZE_Y * 32) {
+        run = false;        
+        printf("You lost\n");
+    }
 }
 
 void AddCollisionTileAt(Uint32 x, Uint32 y)
@@ -172,16 +197,41 @@ void AddCollisionTileAt(Uint32 x, Uint32 y)
     map[x][y].isSolid = true;
 }
 
+char* ReadMapFile()
+{
+    char* data = malloc(1000);
+    mapFile = fopen("/Users/markl/dev/platformer/data/map1.txt", "r");
+    int count = 0;
+    char ch = 0;
+    for (int i=0;i<1000;i++) {
+        char ch = fgetc(mapFile);
+        data[count++] = ch;
+        if (ch == 'e') break;
+    }
+    data[count] = '\0';
+    return data;
+}
+
 void InitMap()
 {
     memset(&map, 0, sizeof(map));
-    for (int i=0;i<SIZE_X;i++) {
-        AddCollisionTileAt(i, SIZE_Y-1);
+    char* mapData = ReadMapFile();
+    int row = 0;
+    int col = 0;
+    for (int i=0;i<1000;i++) {
+        if (mapData[i] == 'e') {
+            break;
+        } else if (mapData[i] == '\n') {
+            col = 0;
+            row++;
+        } else if (mapData[i] == 'a') {
+            col++;
+        } else if (mapData[i] == 'b') {
+            AddCollisionTileAt(col++, row);
+        }
     }
-    AddCollisionTileAt(15, SIZE_Y-2);
-    AddCollisionTileAt(5, SIZE_Y-2);
-    AddCollisionTileAt(10, SIZE_Y-4);
-    AddCollisionTileAt(7, SIZE_Y-6);
+    fclose(mapFile);
+    free(mapData);
 }
 
 int main(int argc, char* argv[])
@@ -213,7 +263,6 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    bool run = true;
     SDL_Event event;
 
     InitPlayer();
@@ -237,32 +286,42 @@ int main(int argc, char* argv[])
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym) {
                         case SDLK_LEFT:
+                            if (player.direction == RIGHT) {
+                                player.vx = 0;
+                            }
                             player.ax = -PLAYER_X_ACC;
+                            player.direction = LEFT;
                             break;
                         case SDLK_RIGHT:
+                            if (player.direction == LEFT) {
+                                player.vx = 0;
+                            }
                             player.ax = PLAYER_X_ACC;
+                            player.direction = RIGHT;
                             break;
                         case SDLK_SPACE:
-                            if (player.isOnGround) {
+                            if (player.isOnGround && player.canJumpAgain) {
                                 player.vy = -PLAYER_JUMP_VEL;
                                 player.isOnGround = false;
+                                player.canJumpAgain = false;
                             }
                     }
                     break;
                 case SDL_KEYUP:
                     switch (event.key.keysym.sym) {
                         case SDLK_LEFT:
-                            if (player.vx < 0) {
-                                player.vx = 0;
-                                player.ax = 0;
+                            if (player.direction == LEFT) {
+                                player.ax = PLAYER_X_DEC;
                             }
                             break;
                         case SDLK_RIGHT:
-                            if (player.vx > 0) {
-                                player.vx = 0;
-                                player.ax = 0;
+                            if (player.direction == RIGHT) {
+                                player.ax = -PLAYER_X_DEC;
                             }
-                        break;
+                            break;
+                        case SDLK_SPACE:
+                            player.canJumpAgain = true;
+                            break;
                     }
                     break;
             }
